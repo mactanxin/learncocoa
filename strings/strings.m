9642b3b5f3bd861015923cf6616180856efe38db
uc0079 <naruto97d@gmail.com>
2010-03-26 15:51:58 +0800

    关于字符串的子串处理

--- strings/strings.m
+++ strings/strings.m
@@ -6,6 +6,18 @@ int main (int argc, const char * argv[]) {
     // insert code here...
     NSString * artist = @"Underworld";
 	NSLog(@"%@ has length: %d", artist, [artist length]);
+	
+	/* 对子字符串的处理 */
+	NSString * song = @"Let Forever Be,The Chemical Brothers";
+	NSRange range = [song rangeOfString:@","];
+	printf("Be location: %i\n", range.location); 
+	
+	NSString * title = [song substringToIndex:range.location];
+	NSString * singer = [song substringToIndex:range.location + range.length];
+	
+	printf("title: %s\n", [title UTF8String]);
+	printf("singer: %s\n", [singer UTF8String]);
+	
     [pool drain];
     return 0;
 }
