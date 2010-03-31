cae3ca5f95fefaaee6c6ae18c69d5614fcfc5a5f
uc0079 <naruto97d@gmail.com>
2010-03-31 11:10:39 +0800

    可变字典

--- dict/dict.m
+++ dict/dict.m
@@ -9,7 +9,13 @@ int main (int argc, const char * argv[]) {
 	NSArray * value = [@"alpha beta charlie delta" componentsSeparatedByString:@" "];
 	NSDictionary * dict = [[NSDictionary alloc] initWithObjects:value forKeys:keys];
 	printf("%s\n",[[dict description]UTF8String]);
-
+	
+	NSMutableDictionary * md = [[NSMutableDictionary alloc] initWithObjects:value forKeys:keys];
+	printf("%s\n",[[md description] UTF8String]);
+	[md setObject:@"hello" forKey:@"world"];
+	printf("%s\n",[[md description] UTF8String]);
+	[md removeObjectForKey:@"two"];
+	printf("%s\n",[[md description] UTF8String]);
     [pool drain];
     return 0;
 }
